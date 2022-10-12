48fc0792a5f9d94e0c8c00958bceb247d54cddcd
ebite zion <ebitezion@gmail.com>
2022-10-12 20:07:27 +0100

    update to fix get logic

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -13,14 +13,14 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
-	winners, err := data.ListAllJSON()
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 
 	year := req.URL.Query().Get("year")
 	if year == "" {
+		winners, err := data.ListAllJSON()
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		res.Write(winners)
 	} else {
 		filteredWinners, err := data.ListAllByYear(year)
